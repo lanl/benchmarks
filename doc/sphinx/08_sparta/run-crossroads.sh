09e01d18ce23672eae9d559b91811c22c33bb6a7
Anthony Agelastos <amagela@sandia.gov>
2023-09-25 10:18:37 -0600

    more mods to run-crossroads.sh script for SPARTA

--- doc/sphinx/08_sparta/run-crossroads.sh
+++ doc/sphinx/08_sparta/run-crossroads.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=sparta-L2-S0
 #SBATCH --nodes=1
 #SBATCH --time=4:00:00
-#SBATCH --core-spec=0
+#SBATCH --core-spec=0  # this seemingly needs to be done with sbatch
 #SBATCH --exclude=nid006219,nid005850,nid003658,nid001813,nid001451,nid002233,nid005892,nid005896,nid003804,nid002065,nid001855,nid005912,nid006402,nid005723,nid006615,nid005851,nid005356
 
 umask 022
@@ -27,6 +27,8 @@ export TMPDIR="/tmp/$SLURM_JOB_ID"  # set to avoid potential runtime error
 # SPARTA setup
 export APP_EXE="${DIR_EXE}/spa_crossroads_omp"
 export APP_REPEAT=1
+export SPARTA_RUN=500
+export SPARTA_PPC=47
 
 # MPI setup
 export NODES=1
@@ -35,14 +37,14 @@ export THREADS_PER_RANK=1
 
 # OpenMP setup
 export THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=true
+# export OMP_PLACES=threads
+# export OMP_PROC_BIND=true
 export OMP_NUM_THREADS=${THREADS_PER_RANK}
 
 # LDPXI setup if applicable
 export LDPXI_INST="gru,rank"
 export LDPXI_OUTPUT="sparta-ldpxi.$SLURM_JOB_ID.csv"
-export LDPXI_RANK_OUTPUT="sparta-ldpxi.$SLURM_JOB_ID.%d.csv"
+# export LDPXI_RANK_OUTPUT="sparta-ldpxi.$SLURM_JOB_ID.%d.csv"
 
 echo "################"
 echo "INFO: System and environment information"
@@ -59,10 +61,13 @@ hostname
 echo "    INFO: NUMA info"
 numactl --hardware
 
-# create and populate run folder
+# create and populate run folder and edit input file
 mkdir -p "${DIR_BASE}/${DIR_RUN}"
 cd "${DIR_BASE}/${DIR_RUN}"
-cp -a "${DIR_SRC}/examples/cylinder/in.cylinder" ./
+# cp -a "${DIR_SRC}/examples/cylinder/in.cylinder" ./
+awk "\$1 ~ /^run\$/ {\$2 = ${SPARTA_RUN}}1" "${DIR_SRC}/examples/cylinder/in.cylinder" \
+    | awk "\$2 ~ /^ppc\$/ {\$4 = ${SPARTA_PPC}}1" \
+    > "./in.cylinder"
 cp -a "${DIR_SRC}/examples/cylinder/circle_R0.5_P10000.surf" ./
 cp -a "${DIR_SRC}"/examples/cylinder/air.* ./
 
@@ -70,16 +75,19 @@ for (( i=0; i<${APP_REPEAT}; i++ )) ; do
     echo "################"
     echo "INFO: Perform Simulation #{i}"
     # export LD_PRELOAD=libldpxi_mpi.so
+    export LD_PRELOAD=/usr/projects/hpctest/amagela/ldpxi/ldpxi/install/ats3/ldpxi-1.0.1/intel+cray-mpich-8.1.25/lib/libldpxi_mpi.so.1.0.1
     time srun \
-        --ntasks-per-node=$RANKS_PER_NODE \
+        --ntasks=$RANKS_PER_NODE \
         --cpus-per-task=2 \
         --cpu-bind=verbose \
+        --distribution=block:block \
         --output="${FILE_LOG//.log/-${i}.log}" \
             "${APP_EXE}" \
-            -in "in.cylinder" \
+            -in "in.cylinder"
+        hostname  
     unset LD_PRELOAD
 done
-#         numactl --physcpubind=112-223 \  # this avoids all LNM conflicts
+#         numactl --physcpubind=112-223 hostname  
                         
 mv ../slurm-${SLURM_JOBID}.out .
 
