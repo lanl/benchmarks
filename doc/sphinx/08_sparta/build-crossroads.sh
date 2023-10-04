a8df93b6eb500f02f30e9991aa03772fc1b4e34f
Anthony Agelastos <amagela@sandia.gov>
2023-10-03 20:42:25 -0600

    updated build script for SPARTA

--- doc/sphinx/08_sparta/build-crossroads.sh
+++ doc/sphinx/08_sparta/build-crossroads.sh
@@ -4,22 +4,38 @@ umask 022
 set -e
 set -x
 
-dir_root=`git rev-parse --show-toplevel`
+dir_root="`git rev-parse --show-toplevel`"
 dir_src="${dir_root}/sparta"
+dir_base="` pwd -P `"
 
 module list
 
+# do the -march=sapphirerapids build
 pushd "${dir_src}"
 git clean -fdx
 git reset --hard
 popd
-cp -a Makefile.crossroads_omp "${dir_src}/src/MAKE"
-
+cp -a Makefile.crossroads_omp_spr "${dir_src}/src/MAKE"
 pushd "${dir_src}/src"
 make yes-kokkos
-make -j 16 crossroads_omp
+make -j 16 crossroads_omp_spr
 echo "Resultant build info:"
-ls -lh `pwd -P`/spa_crossroads_omp
+ls -lh "`pwd -P`/spa_crossroads_omp_spr"
+mv "`pwd -P`/spa_crossroads_omp_spr" "${dir_base}"
+popd
+
+# do the AVX512 build
+pushd "${dir_src}"
+git clean -fdx
+git reset --hard
+popd
+cp -a Makefile.crossroads_omp_skx "${dir_src}/src/MAKE"
+pushd "${dir_src}/src"
+make yes-kokkos
+make -j 16 crossroads_omp_skx
+echo "Resultant build info:"
+ls -lh "`pwd -P`/spa_crossroads_omp_skx"
+mv "`pwd -P`/spa_crossroads_omp_skx" "${dir_base}"
 popd
 
 
