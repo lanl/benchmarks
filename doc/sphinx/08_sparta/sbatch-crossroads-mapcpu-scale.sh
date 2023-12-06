a8bc399c13572720860a6369ddc84cf4dcc04767
Anthony Agelastos <amagela@sandia.gov>
2023-12-06 11:10:22 -0700

    added scripts for launching SPARTA on more than 1 node

---
+++ doc/sphinx/08_sparta/sbatch-crossroads-mapcpu-scale.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This submits a full sweep
+
+export NUM_DUPLICATES=1
+export APP_REPEAT=1
+export SBATCH_OPTS="--core-spec=0 --partition=hbm"
+
+for (( i=0 ; i<${NUM_DUPLICATES} ; i++ )) ; do
+
+    export SPARTA_IS_KOKKOS_TOOLS="no"
+
+    export APP_NAME="spa_crossroads_serial_spr"
+
+    export NODES=1
+    export SPARTA_PPC=35
+    export RANKS_PER_DOMAIN=14
+    export SPARTA_RUN=6866
+    export SPARTA_STATS=100
+    sleep 0.2
+    sbatch --nodes=${NODES} ${SBATCH_OPTS} scale-crossroads-mapcpu.sh
+
+done
+
+exit 0
