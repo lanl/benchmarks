0b66eebadef4ac87189d1fec6133fbad1caa147e
Anthony Agelastos <amagela@sandia.gov>
2023-12-06 10:21:39 -0700

    tweaked scripts to focus on Serial backend for Crossroads

--- doc/sphinx/08_sparta/build-crossroads.sh
+++ doc/sphinx/08_sparta/build-crossroads.sh
@@ -10,32 +10,18 @@ dir_base="` pwd -P `"
 
 module list
 
-# do the AVX512 build
-pushd "${dir_src}"
-git clean -fdx
-git reset --hard
-popd
-cp -a Makefile.crossroads_omp_skx "${dir_src}/src/MAKE"
-pushd "${dir_src}/src"
-make yes-kokkos
-make -j 16 crossroads_omp_skx
-echo "Resultant build info:"
-ls -lh "`pwd -P`/spa_crossroads_omp_skx"
-cp -a "`pwd -P`/spa_crossroads_omp_skx" "${dir_base}"
-popd
-
 # do the -march=sapphirerapids build
 pushd "${dir_src}"
 git clean -fdx
 git reset --hard
 popd
-cp -a Makefile.crossroads_omp_spr "${dir_src}/src/MAKE"
+cp -a Makefile.crossroads_serial_spr "${dir_src}/src/MAKE"
 pushd "${dir_src}/src"
 make yes-kokkos
-make -j 16 crossroads_omp_spr
+make -j 16 crossroads_serial_spr
 echo "Resultant build info:"
-ls -lh "`pwd -P`/spa_crossroads_omp_spr"
-cp -a "`pwd -P`/spa_crossroads_omp_spr" "${dir_base}"
+ls -lh "`pwd -P`/spa_crossroads_serial_spr"
+cp -a "`pwd -P`/spa_crossroads_serial_spr" "${dir_base}"
 popd
 
 # build map_cpu for binding
@@ -43,7 +29,7 @@ pushd "${dir_root}/map_cpu"
 gcc -O2 -o map_cpu map_cpu.c
 popd
 
-# build Kokkos Tools for memory information
+# build Kokkos Tools for memory information (if desired)
 pushd "${dir_root}/kokkos-tools/profiling/space-time-stack"
 make CXX=CC
 popd
