05657cc01b15b602cd1c3509e51019dee77a8ef7
Anthony Agelastos <amagela@sandia.gov>
2024-01-23 12:52:16 -0700

    updated build script to also handle running the prep script

--- miniem_build/build-crossroads.sh
+++ miniem_build/build-crossroads.sh
@@ -18,6 +18,17 @@ pushd spack
 git apply "${DIR_BASE}/spack-fixes-v0.21.0.patch"
 popd
 
+# create spack.yaml from template.yaml
+if test -f "${DIR_BASE}/prep-spack-yaml.sh" ; then
+    sh "${DIR_BASE}/prep-spack-yaml.sh"
+fi
+
+# exit if spack.yaml not present
+if test ! -f "${DIR_BASE}/spack.yaml" ; then
+    echo "ERROR: spack.yaml not present!"
+    exit 1
+fi
+
 # load Spack into environment
 . ./spack/share/spack/setup-env.sh
 
