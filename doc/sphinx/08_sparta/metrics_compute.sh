827cfc358048c8afad23f9beacbb14499ebee7a0
Anthony Agelastos <amagela@sandia.gov>
2024-03-14 22:02:34 -0600

    updated numbers on crossroads

--- doc/sphinx/08_sparta/metrics_compute.sh
+++ doc/sphinx/08_sparta/metrics_compute.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-find . -name "log.sparta" -type f -print0 \
-    | xargs -0 -P 8 -I mfl ./sparta_fom.py --file mfl --numRanksPerNode 112
+compute_fom()
+{
+    num_ranks=` grep Running $1 | awk '{print $3}' `
+    ./sparta_fom.py --file $1 --numRanksPerNode $num_ranks
+}
+export -f compute_fom
+
+find $1 -name "log.sparta" -type f -print0 \
+    | xargs -0 -P 8 -I mfl bash -c "compute_fom mfl"
 
 exit 0
