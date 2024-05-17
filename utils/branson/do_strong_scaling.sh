eef73b0545aa406aca9008eb7ac65884cbf178af
Galen M. Shipman <Galen Shipman>
2024-05-17 16:12:13 -0600

    fixes for strong scaling script

--- utils/branson/do_strong_scaling.sh
+++ utils/branson/do_strong_scaling.sh
@@ -1,14 +1,17 @@
-export CALI_CONFIG=sample-report
 
-cp ../inputs/3D_hohlraum_single_node_cpu.xml . 
+input=3D_hohlraum_single_node_cpu.xml
+cp ../inputs/3D_hohlraum_single_node.xml ./${input}
+
+sed -i 's/<use_gpu_transporter>TRUE<\/use_gpu_transporter>/<use_gpu_transporter>FALSE<\/use_gpu_transporter>/g' ${input}
+
 for s in 10 66 200
 do
-  sed -i 's/<photons>.*<\/photons>/<photons>'${s}'000000\<\/photons>/g' 3D_hohlraum_single_node_cpu.xml 
+  sed -i 's/<photons>.*<\/photons>/<photons>'${s}'000000\<\/photons>/g' ${input}
  #4 12 16 24 32 48  #4 8 16 32 36 64 72
-  for i in 4 12 16 24 32 48 96 192
+  for i in 8 32 56 88 112
   do
-    grep photons 3D_hohlraum_single_node_cpu.xml 
+    grep photons ${input}
     #mpirun -mca coll basic,self,libnbc  -n $i ./BRANSON ./3D_hohlraum_single_node_cpu.xml | tee run.$s.$i.out
-    srun -n $i -m block:block --hint=nomultithread ./BRANSON ./3D_hohlraum_single_node_cpu.xml | tee run.$s.$i.out
+    srun -n $i -m block:block --hint=nomultithread ./BRANSON ./${input} | tee run.$s.$i.out
   done
 done
